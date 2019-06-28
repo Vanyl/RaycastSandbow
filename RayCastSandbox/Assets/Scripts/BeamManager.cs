e6ff1c91d07b71ad6b9d01c08dd8e191d71e846d
Yavanna Perez <yavannaperez@hotmail.com>
2019-06-28 14:37:29 +0200

    shoot disparition + shoot on click

---
+++ RayCastSandbox/Assets/Scripts/BeamManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+//                          ^ nom de la classe qu'on veut
+
+
+public class BeamManager : MonoBehaviour
+{
+    private LineRenderer line;
+
+    [Tooltip("Time before destruction.")]
+    public float time = 3f;
+
+    [Header("Position")] //Dans script en public est noté dans unity (plus strcturé)
+    public Vector3 start;
+    public Vector3 end;
+    
+    void Start()
+    {
+        line = GetComponent<LineRenderer>();
+        Destroy(gameObject, time); //fonction qui détruit objet si on met pas de temps c'est immédiat, si on met "après un certain temps destruction"
+
+        line.positionCount = 2;
+        line.SetPosition(0, start);
+        line.SetPosition(1, end);
+
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
---
+++ RayCastSandbox/Assets/Scripts/Shoot.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Vector3 start = transform.position;
+            Vector3 end = transform.position + transform.forward * 1000;
+
+            RaycastHit hit;
+
+            if(Physics.Raycast(transform.position, transform.forward, out hit)) // si je touche
+            {
+                end = hit.point; //alors point de fin = hit.point
+            }
+
+
+
+
+            //charger ressource
+            GameObject resource = Resources.Load("beam") as GameObject;
+            BeamManager beam = resource.GetComponent<BeamManager>();
+            beam.start = start;
+            beam.end = end;
+
+            Instantiate(resource);
+
+
+        }
+        
+    }
+}
