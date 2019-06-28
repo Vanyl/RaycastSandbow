5ac90e92a761329119980c31234c9c3052aa1305
Yavanna Perez <yavannaperez@hotmail.com>
2019-06-28 09:36:44 +0200

    raycast

---
+++ RayCastSandbox/Assets/Scripts/Hit.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hit : MonoBehaviour
+{
+    Vector3 fwd = transform.forward;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawRay(transform.position, fwd, Color.blue);
+
+        if (Physics.Raycast(transform.position, fwd))
+        {
+            print("HIT");
+        }
+
+        else
+        {
+            Debug.DrawRay(transform.position, fwd, Color.red);
+        }
+    }
+}
